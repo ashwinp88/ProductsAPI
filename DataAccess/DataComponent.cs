ad9ca04c3a69112e835abb145fcdefc16187d607
Ashwin <ashwin.purushotaman@hotmail.com>
2019-07-02 07:09:09 -0400

    Initial Commit

---
+++ DataAccess/DataComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Dapper.Contrib;
+
+namespace DataAccess
+{
+    public abstract class DataComponent : IDataComponent
+
+    {
+        public long ID { get; set; }
+
+        public void DeleteRecord()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadRecord()
+        {
+            throw new NotImplementedException();
+        }
+
+        public long SaveRecord()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
