0b84518b80713fb7751a161fb3d17f3900d74b8f
Ashwin <ashwin.purushotaman@hotmail.com>
2019-07-02 23:46:00 -0400

    Added Unit Test Examples

---
+++ ProductsAPITests/ProductControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Products.Core;
+using ProductsAPI.Controllers;
+using System.Web.Http.Results;
+using Products.Core.Repositories;
+using Products.Core.Domain;
+
+namespace ProductsAPITests
+{
+    [TestFixture]
+    class ProductControllerTests
+    {
+        [Test]
+        public void GetProducts_WhenCalled_ReturnsOK()
+        {
+            //Arrange
+            var unitOfWork = new Mock<IUnitOfWork>();
+            var productsRepository = new Mock<IProductRepository>();
+            unitOfWork.Setup(m => m.Products).Returns(productsRepository.Object);
+            var controller = new ProductsController(unitOfWork.Object);
+
+            //Act
+            var result = controller.GetProducts();
+
+            //Assert           
+            unitOfWork.Verify(u => u.Products.GetAll());
+            Assert.That(result, Is.TypeOf<OkNegotiatedContentResult<IEnumerable<Product>>>());
+        }
+    }
+}
---
+++ ProductsAPITests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProductsAPITests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProductsAPITests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e9c36d4-a463-4421-8099-0e3d90944d7f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
